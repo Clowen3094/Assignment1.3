c41b23d1a15153d47d8dfa51ce5a5c6c49039f94
Clowen3094 <kaninia207@gmail.com>
2017-05-30 11:48:58 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,14 @@
+class New {
+ int a = 20;
+ int b = 10;
+public static void main(String args[]){
+
+New obj = new New();
+int b = obj.a;
+System.out.println("New value of b is: " + b);
+
+int a = obj.b;
+System.out.println("New value of a is: " + a);
+
+}
+}
